31fdc3d93995da0cc224a6d39fc173ea5dddfe38
Kevin Lewis Hall <info@insitderp.com>
2022-12-04 01:11:56 +0100

    Migration error duplicate sessions table

--- database/migrations/2022_11_22_222340_create_sessions_table.php
+++ database/migrations/2022_11_22_222340_create_sessions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('sessions', function (Blueprint $table) {
+        Schema::create('sessionsextra', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('sessionsextra');
     }
 };
